ba6d9c4e026ebe938e21a9de4f1de7e8af7fad0c
drei109 <drei.rar@gmail.com>
2019-07-03 10:11:14 -0500

    working on portafolio

---
+++ SistemaPortafolio/Models/Portafolio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaPortafolio.Models
+{
+    [Table("Portafolio")]
+    public class Portafolio
+    {
+        [Key]
+        public int portafolio_id { get; set; }
+
+        [Required]
+        public int cursodocente_id { get; set; }
+
+        [StringLength(10)]
+        public string unidad { get; set; }
+    }
+}
